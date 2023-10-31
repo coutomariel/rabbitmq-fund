bbfbd8f5f93ca9cfdcdb8b3ad1fe8692981cf8d3
Mariel <coutomariel@gmail.com>
2023-10-31 13:56:22 -0300

    Initial commit producer

---
+++ consumer-service/src/test/kotlin/com/coutomariel/consumerservice/ConsumerServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coutomariel.consumerservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConsumerServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
